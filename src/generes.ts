0860f1092e59d7c0f2772c60e1f2908831b4ef05
Rikardo Rodríguez <maryxtryx@gmail.com>
2025-02-21 13:26:24 +0100

    Update 21/02/2025

---
+++ src/generes.ts
@@ -0,0 +1,65 @@
+import type { Connection, ResultSetHeader } from 'mysql2/promise';
+import type { GenereRow } from './entities';
+
+export class ManageGeneres {
+    constructor(private connection: Connection) {}
+
+    async getAllGeneres(): Promise<GenereRow[]> {
+        const q = 'select genere_id as id, name from generes';
+        const [rows] = await this.connection.query<GenereRow[]>(q);
+        return rows;
+    }
+
+    async getGenereById(id: number) {
+        const q = `select genere_id as id, name from generes where genere_id = ?`;
+        const [rows] = await this.connection.query<GenereRow[]>(q, [id]);
+
+        if (rows.length === 0) {
+            throw new Error(`Genere with id ${id} not found`);
+        }
+        return rows[0];
+    }
+
+    async createGenere(name: string) {
+        const q = `insert into generes (name) VALUES (?);`;
+        const [result] = await this.connection.query<ResultSetHeader>(q, [
+            name,
+        ]);
+
+        if (result.affectedRows === 1) {
+            console.log('Genere created with id:', result.insertId);
+            return this.getGenereById(result.insertId);
+        }
+
+        throw new Error('Genere not created');
+    }
+
+    async updateGenere(id: number, name: string) {
+        const q = `update generes set name = ? where genere_id = ?;`;
+        const [result] = await this.connection.query<ResultSetHeader>(q, [
+            name,
+            id,
+        ]);
+
+        if (result.affectedRows === 1) {
+            console.log('Genere updated with id:', id);
+            return this.getGenereById(id);
+        }
+
+        throw new Error('Genere not updated');
+    }
+
+    async deleteGenere(id: number) {
+        const genereForDelete = await this.getGenereById(id);
+
+        const q = `delete from generes where genere_id = ?;`;
+        const [result] = await this.connection.query<ResultSetHeader>(q, [id]);
+
+        if (result.affectedRows === 1) {
+            console.log('Genere deleted with id:', id);
+            return genereForDelete;
+        }
+
+        throw new Error('Genere not deleted');
+    }
+}
